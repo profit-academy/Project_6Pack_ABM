d935f734792204aa833d8b01e99f71a73d9d322b
gym <dinhan.btre123@gmail.com>
2026-09-18 16:09:44 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,4 @@
-// Thay thế bằng thông tin thực tế từ Supabase Project của bạn
-// Dashboard > Settings > API
 export const CONFIG = {
-    SUPABASE_URL: "https://ahrusdraiwnlxvtbyvva.supabase.co/rest/v1/",
+    SUPABASE_URL: "https://ahrusdraiwnlxvtbyvva.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFocnVzZHJhaXdubHh2dGJ5dnZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3MTQ2MTAsImV4cCI6MjEwNTI5MDYxMH0.Xvz-Cz4EcUgRfAC_otpd2z5ylAYtd432tqgBtDLFpqE"
 };
